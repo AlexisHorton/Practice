54592fd213e1c156ddbfd4ef175b46ebc7006d28
alexis <alexisd.horton@gmail.com>
2022-02-22 13:10:57 -0500

    Initial Commit

---
+++ HandoutPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace HandoutPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // BASIC FUNCTIONS: 
+
+            //  *EXCERCISE 1* //
+
+            Console.WriteLine("Enter some text!:");
+            string response = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(response);
+            //
+
+            // *EXCERCISE 2* //
+            Console.Write("Enter ANY number you want:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Here's your number plus 1! :{num + 1}");
+        }
+    }
+}
